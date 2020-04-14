c2041f5ceb2144c7c2c6f28061fdac4d0233be46
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-04-14 11:07:59 +0000

    Have added database and working on live browser

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Rundle18@myfirstcluster-6cjbt.mongodb.net/cook_book?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
+@app.route('/recipes')
 def hello():
-    return "Cook Book"
+    recipes = mongo.db.recipes.find()
+    return render_template("base.html", recipes=recipes)
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
             debug=True)
